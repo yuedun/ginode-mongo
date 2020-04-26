48a592cfc61fdccb56e1b990c8274af42557db86
hale.huo <hale.huo@zhangmen.com>
2020-04-26 23:32:04 +0800

    添加忽略tag

--- pkg/component/component-model.go
+++ pkg/component/component-model.go
@@ -11,30 +11,30 @@ type Component struct {
 	ID            primitive.ObjectID `json:"_id" bson:"_id,omitempty"`             //bson是用来创建后返回，omitempty可选
 	Name          string             `json:"name" bson:"name,omitempty"`           //组件名
 	Category      string             `json:"category" bson:"category,omitempty"`   //分类
-	Title1        string             `json:"title_1" bson:"title_1"`               //一级标题
-	Title2        string             `json:"title_2" bson:"title_2"`               //二级标题
-	Title3        string             `json:"title_3" bson:"title_3"`               //三级标题
-	Description   string             `json:"description" bson:"description"`       //文字描述
-	BackgroundImg string             `json:"background_img" bson:"background_img"` //背景图
-	BigImg        string             `json:"big_img" bson:"big_img"`               //展示大图
+	Title1        string             `json:"title_1,omitempty" bson:"title_1"`               //一级标题
+	Title2        string             `json:"title_2,omitempty" bson:"title_2"`               //二级标题
+	Title3        string             `json:"title_3,omitempty" bson:"title_3"`               //三级标题
+	Description   string             `json:"description,omitempty" bson:"description"`       //文字描述
+	BackgroundImg string             `json:"background_img,omitempty" bson:"background_img"` //背景图
+	BigImg        string             `json:"big_img,omitempty" bson:"big_img"`               //展示大图
 	Elements      []Element          `json:"elements" bson:"elements,omitempty"`   //多个同类型元素列表展示
-	Links         []Link             `json:"links" bson:"links,omitempty"`         //链接或按钮
-	Extras        []Component        `json:"extras" bson:"extras,omitempty"`       //其他补充内容
-	Status        int                `json:"status" bson:"status,omitempty"`       //可用状态
+	Links         *[]Link             `json:"links,omitempty" bson:"links,omitempty"`         //链接或按钮
+	Extras        *[]Component        `json:"extras,omitempty" bson:"extras,omitempty"`       //其他补充内容
+	Status        int                `json:"status,omitempty" bson:"status,omitempty"`       //可用状态
 	Sort          int                `json:"sort" bson:"sort,omitempty"`           //排序
-	CreatedAt     time.Time          `json:"created_at" bson:"created_at,omitempty"`
-	UpdatedAt     time.Time          `json:"updated_at" bson:"updated_at,omitempty"`
+	CreatedAt     time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt     time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
 
 //Element 组件中包含的同类型元素
 type Element struct {
-	Icon        string `json:"icon" bson:"icon,omitempty"` //小图标
-	Img         string `json:"img" bson:"img,omitempty"`   //大图
-	Title1      string `json:"title_1" bson:"title_1,omitempty"`
-	Title2      string `json:"title_2" bson:"title_2,omitempty"`
-	Title3      string `json:"title_3" bson:"title_3,omitempty"`
-	Description string `json:"description" bson:"description,omitempty"`
-	Link        string `json:"link" bson:"link,omitempty"` //跳转链接，也可以用作视频链接
+	Icon        string `json:"icon,omitempty" bson:"icon,omitempty"` //小图标
+	Img         string `json:"img,omitempty" bson:"img,omitempty"`   //大图
+	Title1      string `json:"title_1,omitempty" bson:"title_1,omitempty"`
+	Title2      string `json:"title_2,omitempty" bson:"title_2,omitempty"`
+	Title3      string `json:"title_3,omitempty" bson:"title_3,omitempty"`
+	Description string `json:"description,omitempty" bson:"description,omitempty"`
+	Link        string `json:"link,omitempty" bson:"link,omitempty"` //跳转链接，也可以用作视频链接
 }
 
 //Link 组件包含的链接
