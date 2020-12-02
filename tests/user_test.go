f45c50610b717bb0cca71da9969e6e7eb6124edf
hale.huo <hale.huo@zhangmen.com>
2020-12-02 18:11:01 +0800

    测试初始化

--- tests/user_test.go
+++ tests/user_test.go
@@ -1,14 +1,43 @@
 package tests
 
 import (
+	"context"
+	"fmt"
+	"log"
 	"testing"
+	"time"
 
 	"github.com/yuedun/ginode-mongo/db"
 	"github.com/yuedun/ginode-mongo/pkg/page"
 	"github.com/yuedun/ginode-mongo/pkg/user"
 	"github.com/yuedun/ginode-mongo/util"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+func TestMain(m *testing.M) {
+	c, err := util.GetConf("../conf.yaml")
+	if err != nil {
+		panic(err)
+	}
+	client, err := mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%v:%v@%v/%v", c.User, c.Pwd, c.Host, c.Dbname)))
+	if err != nil {
+		panic(err)
+	}
+	ctx, cancel := context.WithTimeout(context.TODO(), 20*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		panic(err)
+	}
+	if err = client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+	log.Println("Connected to MongoDB!")
+	db.Connect = client
+}
 
 func TestGetUser(t *testing.T) {
 	userService := user.NewService(db.NewDB("website"))
